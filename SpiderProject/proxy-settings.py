c8f360fb737741f7b451f39152a5136c0926eb26
jidongdeatao <972395203@qq.com>
2018-08-25 00:46:05 -0700

    Create proxy-settings.py

---
+++ SpiderProject/proxy-settings.py
@@ -0,0 +1,126 @@
+#多种认证方式
+#copy from https://github.com/Python3WebSpider/ProxySettings/
+#1
+#urllib_socks.py
+# pip3 install PySocks
+
+import socks
+import socket
+from urllib import request
+from urllib.error import URLError
+
+socks.set_default_proxy(socks.SOCKS5, '127.0.0.1', 9742)
+socket.socket = socks.socksocket
+try:
+    response = request.urlopen('http://httpbin.org/get')
+    print(response.read().decode('utf-8'))
+except URLError as e:
+    print(e.reason)
+
+
+#2
+#urllib_no_auth.py
+from urllib.error import URLError
+from urllib.request import ProxyHandler, build_opener
+
+proxy = '127.0.0.1:9743'
+
+proxy_handler = ProxyHandler({
+    'http': 'http://' + proxy,
+    'https': 'https://' + proxy
+})
+opener = build_opener(proxy_handler)
+try:
+    response = opener.open('http://httpbin.org/get')
+    print(response.read().decode('utf-8'))
+except URLError as e:
+    print(e.reason)
+
+
+#3
+#urllib_auth.py
+from urllib.error import URLError
+from urllib.request import ProxyHandler, build_opener
+
+proxy = 'username:password@127.0.0.1:9743'
+
+proxy_handler = ProxyHandler({
+    'http': 'http://' + proxy,
+    'https': 'https://' + proxy
+})
+opener = build_opener(proxy_handler)
+try:
+    response = opener.open('http://httpbin.org/get')
+    print(response.read().decode('utf-8'))
+except URLError as e:
+    print(e.reason)
+
+
+#4
+#requests_socks.py
+import requests
+
+proxy = '127.0.0.1:9742'
+proxies = {
+    'http': 'socks5://' + proxy,
+    'https': 'socks5://' + proxy
+}
+try:
+    response = requests.get('http://httpbin.org/get', proxies=proxies)
+    print(response.text)
+except requests.exceptions.ConnectionError as e:
+    print('Error', e.args)
+    
+
+#5
+#reqeusts_socks_way2.py
+# pip3 install PySocks
+import requests
+import socks
+import socket
+
+socks.set_default_proxy(socks.SOCKS5, '127.0.0.1', 9742)
+socket.socket = socks.socksocket
+try:
+    response = requests.get('http://httpbin.org/get')
+    print(response.text)
+except requests.exceptions.ConnectionError as e:
+    print('Error', e.args)
+    
+#6
+#requests_auth.py
+import requests
+
+proxy = 'username:password@127.0.0.1:9743'
+
+proxies = {
+    'http': 'http://' + proxy,
+    'https': 'https://' + proxy,
+}
+try:
+    response = requests.get('http://httpbin.org/get', proxies=proxies)
+    print(response.text)
+except requests.exceptions.ConnectionError as e:
+    print('Error', e.args)
+    
+    
+#7
+#requests_noauth.py
+import requests
+
+proxy = '127.0.0.1:9743'
+
+proxies = {
+    'http': 'http://' + proxy,
+    'https': 'https://' + proxy,
+}
+try:
+    response = requests.get('http://httpbin.org/get', proxies=proxies)
+    print(response.text)
+except requests.exceptions.ConnectionError as e:
+    print('Error', e.args)
+    response = opener.open('http://httpbin.org/get')
+    response = opener.open('http://httpbin.org/get')
+    
+#8
+#
