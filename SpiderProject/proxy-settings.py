2cf2f4dfafe6caa03e40289d785bad98612178ab
jidongdeatao <972395203@qq.com>
2018-08-25 00:50:25 -0700

    Update proxy-settings.py

--- SpiderProject/proxy-settings.py
+++ SpiderProject/proxy-settings.py
@@ -218,4 +218,15 @@ print(browser.page_source)
 
 
 #11
-#1
+#selenium_phantomjs_auth.py
+from selenium import webdriver
+
+service_args = [
+    '--proxy=127.0.0.1:9743',
+    '--proxy-type=http',
+    '--proxy-auth=username:password'
+]
+
+browser = webdriver.PhantomJS(service_args=service_args)
+browser.get('http://httpbin.org/get')
+print(browser.page_source)
